ef778caf310d1f7cdfd0f74fa330f6ae8134b8ec
gcorujo <corujo.gabriel@gmail.com>
2024-07-03 22:32:51 -0300

    Added obsidian plugin

---
+++ lua/custom/plugins/obsidian.lua
@@ -0,0 +1,94 @@
+return {
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+  --   "BufReadPre path/to/my-vault/**.md",
+  --   "BufNewFile path/to/my-vault/**.md",
+  -- },
+  dependencies = {
+    -- Required.
+    'nvim-lua/plenary.nvim',
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'personal',
+        path = '/mnt/c/Users/Gabriel/Documents/obsidian/gabriel-vault',
+      },
+    },
+  },
+
+  -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+  completion = {
+    -- Set to false to disable completion.
+    nvim_cmp = true,
+    -- Trigger completion at 2 chars.
+    min_chars = 2,
+  },
+
+  -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+  -- way then set 'mappings = {}'.
+  mappings = {
+    -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+    ['gf'] = {
+      action = function()
+        return require('obsidian').util.gf_passthrough()
+      end,
+      opts = { noremap = false, expr = true, buffer = true },
+    },
+    -- Toggle check-boxes.
+    ['<leader>ch'] = {
+      action = function()
+        return require('obsidian').util.toggle_checkbox()
+      end,
+      opts = { buffer = true },
+    },
+    -- Smart action depending on context, either follow link or toggle checkbox.
+    ['<cr>'] = {
+      action = function()
+        return require('obsidian').util.smart_action()
+      end,
+      opts = { buffer = true, expr = true },
+    },
+  },
+
+  -- Where to put new notes. Valid options are
+  --  * "current_dir" - put new notes in same directory as the current buffer.
+  --  * "notes_subdir" - put new notes in the default notes subdirectory.
+  new_notes_location = 'current_dir',
+
+  picker = {
+    -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+    name = 'telescope.nvim',
+    -- Optional, configure key mappings for the picker. These are the defaults.
+    -- Not all pickers support all mappings.
+    mappings = {
+      -- Create a new note from your query.
+      new = '<C-x>',
+      -- Insert a link to the selected note.
+      insert_link = '<C-l>',
+    },
+  },
+
+  -- Optional, sort search results by "path", "modified", "accessed", or "created".
+  -- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
+  -- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
+  sort_by = 'modified',
+  sort_reversed = true,
+
+  -- Set the maximum number of lines to read from notes on disk when performing certain searches.
+  search_max_lines = 1000,
+
+  -- Optional, determines how certain commands open notes. The valid options are:
+  -- 1. "current" (the default) - to always open in the current window
+  -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
+  -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
+  open_notes_in = 'current',
+}
