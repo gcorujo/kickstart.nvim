941755e765bea863de06b948724614f54941d400
gcorujo <gcorujo@tilsor.com.uy>
2024-07-04 10:19:10 -0300

    Mapped jk to Esc

--- init.lua
+++ init.lua
@@ -157,6 +157,9 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Set jk to <Esc>
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true })
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
