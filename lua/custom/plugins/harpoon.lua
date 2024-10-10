051bd4d918547279caa2cb3d23863f07caf84a6e
gcorujo <gcorujo@tilsor.com.uy>
2024-10-10 14:22:58 -0300

    Updated config of harpoon and oil

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -11,7 +11,7 @@ return {
       function()
         require('harpoon'):list():add()
       end,
-      desc = 'Harpoon File',
+      desc = 'Mark File',
     },
     {
       '<leader>i',
@@ -19,28 +19,28 @@ return {
         local harpoon = require 'harpoon'
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end,
-      desc = 'Harpoon Quick Menu',
+      desc = 'Harpoon Menu',
     },
     {
       '<leader>j',
       function()
         require('harpoon'):list():select(1)
       end,
-      desc = 'Harpoon to file 1',
+      desc = 'Jump to file 1',
     },
     {
       '<leader>k',
       function()
         require('harpoon'):list():select(2)
       end,
-      desc = 'Harpoon to file 2',
+      desc = 'Jump to file 2',
     },
     {
       '<leader>l',
       function()
         require('harpoon'):list():select(3)
       end,
-      desc = 'Harpoon to file 3',
+      desc = 'Jump to file 3',
     },
   },
 }
